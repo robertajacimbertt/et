a8bb278fd4912ca86377dd74f93e4a61e0a133b2
Roberta Jacimbertt <rjacimbertt@gmail.com>
2021-11-17 19:52:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 
 http.createServer(function (request, response) {
    console.log("CHEGOU console.log(params) -> ")
-   console.log(request.params)
    
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end("Hello Robs J");
