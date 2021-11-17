5082646044561ca79e4461275e79ac29bfd21088
Roberta Jacimbertt <rjacimbertt@gmail.com>
2021-11-17 15:12:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function (request, response) {
    target = process.env.TARGET ? process.env.TARGET : 'World' ;
-   msg = process.env.MSG ? process.env.MSG : 'Hello ' + target + '\n';
+   msg = process.env.MSG ? process.env.MSG : 'Hellos ' + target + '\n';
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end(msg);
 }).listen(8080);
