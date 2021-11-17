a23f5bc90a6076187c8b8ac848a42c2ddc05b46a
Roberta Jacimbertt <rjacimbertt@gmail.com>
2021-11-17 11:37:44 -0300

    Create teste-file.js

---
+++ teste-file.js
@@ -0,0 +1,12 @@
+/**
+  *
+  * main() será executado quando você chamar essa ação
+  *
+  * @param As ações do Cloud Functions aceitam um único parâmetro, que deve ser um objeto JSON.
+  *
+  * @return A saída dessa ação, que deve ser um objeto JSON.
+  *
+  */
+function main(params) {
+	return { message: 'Hello World' };
+}
