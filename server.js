4254aeda25bfec24a2159bc3c66ec0e80e729670
Roberta Jacimbertt <rjacimbertt@gmail.com>
2021-11-18 15:30:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-   console.log("CHEGOU console.log(test) -> ", request.param)
+   console.log("CHEGOU console.log(aquiii) -> ", request.method)
+   
+   if (request.method == 'POST') {
+      console.log(" posssssst ")
+   } 
    
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end("Hello Robs J");
