f97bacc0aa6842e886f634b2a4229677419a82a5
Roberta Jacimbertt <rjacimbertt@gmail.com>
2021-11-18 14:57:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-   console.log("CHEGOU console.log(P) -> ", process.env.fofa)
+   console.log("CHEGOU console.log(Pp) -> ", process.env)
    
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end("Hello Robs J");
